33f29762adccd5cc1d1fccd093270999fcfe893d
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-07-25 11:20:19 +0200

    schemas created

---
+++ src/schemas/hr.schema.ts
@@ -0,0 +1,46 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+
+
+@Schema()
+export class HumanResources {
+    @Prop({
+        type: String,
+        required: true,
+    })
+    name: string;
+
+    @Prop({
+        type: String,
+        required: true,
+    })
+    lastName: string;
+
+    @Prop({
+        type: String,
+        unique: true,
+        required: true
+    })
+    email: string;
+
+    @Prop({
+        type: String,
+        required: true,
+    })
+    company: string;
+
+    @Prop({
+        type: Number,
+        min: 1,
+        max: 999
+    })
+    maxReservedStudents: number;
+
+    @Prop({
+        type: String,
+        default: null,
+        required: true
+    })
+    password: string;
+}
+
+export const HrSchema = SchemaFactory.createForClass(HumanResources);
