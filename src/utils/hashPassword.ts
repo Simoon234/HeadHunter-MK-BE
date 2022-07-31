1f42848ad94c2f6ae7a918b0bbf97a972794f126
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-07-31 17:36:11 +0200

    added utils hashPassword and compare password functionality.

--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,4 +1,9 @@
-import { hash } from 'bcrypt';
+import { compare, hash } from "bcrypt";
+
+export const verifyPassword = (password: string, storedPassword: string) => {
+  return compare(password, storedPassword);
+};
+
 
 export const hashPassword = (password: string) => {
   return hash(password, 12);
