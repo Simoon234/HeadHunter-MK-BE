1e1a1ed8290454f171a22f5426cbf16f7044547d
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-06 13:36:50 +0200

    created templates for user and hr registration.

---
+++ src/templates/email/passwordReset.ts
@@ -0,0 +1,13 @@
+export const resetPassword = (
+    name: string,
+    id: string,
+    refreshToken: string,
+) => {
+  return `
+        <h1>Hi ${name}</h1>
+        <p>Forgot your password?</p>
+        <p>We recived a request to reset the password for your account</p>
+        <p>To reset the password click link down below</p>
+        <a href="http://localhost:3000/password-reset/${id}/${refreshToken}">Reset password</a>
+    `;
+};
---
+++ src/templates/email/registration.ts
@@ -0,0 +1,20 @@
+import {ACTIVATION_HR_URL, ACTIVATION_STUDENT_URL} from '../../../config';
+
+const message = (link: string, id: string, token: string) => `
+    <h2> Hi there! </h2>
+    <p> Thank you for signing up for our megaKApp
+    To get you started, please click on the button below to log in to your account for the first time. </p>
+    
+    <a href="${link}/${id}/${token}">I'm here! 👋</a>
+    
+    <p> If you didn’t submit your email address to join our subscriber list, please ignore this message. </p>
+    
+    <p>Regards,
+    The megaKApp support team</p>
+`;
+
+export const registerUser = (id: string, token: string) =>
+    message(ACTIVATION_HR_URL, id, token);
+
+export const registerHr = (id: string, token: string) =>
+    message(ACTIVATION_STUDENT_URL, id, token);
