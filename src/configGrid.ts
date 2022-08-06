2fe1d7a5d93fcad27fa02dc00ef7ea875a8ce945
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-06 13:44:47 +0200

    config, config file.

---
+++ src/configGrid.ts
@@ -0,0 +1,15 @@
+import {mailConfig} from './config/mailConfig';
+
+export const config = {
+  transport: {
+    host: mailConfig.host,
+    secure: mailConfig.secure,
+    auth: {
+      user: mailConfig.auth.user,
+      pass: mailConfig.auth.pass,
+    },
+    tls: {
+      rejectUnauthorized: mailConfig.tls.rejectUnauthorized,
+    },
+  },
+};
