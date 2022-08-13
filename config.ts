6c8b854ffadfe463f20b9918c62a2da4e555879d
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-13 19:03:21 +0200

    config

--- config.ts
+++ config.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 export const ACTIVATION_HR_URL = 'http://localhost:3000/activation/hr';
 export const ACTIVATION_STUDENT_URL =
   'http://localhost:3000/activation/student';
